3e8bd7402cad3f5959f783b719b43fbfab98cfa7
antonella <antonella_caroca@hotmail.com>
2018-08-21 21:05:54 -0300

    test +1

--- test/md-links.test.js
+++ test/md-links.test.js
@@ -1,5 +1,9 @@
 const mdLinks = require('./md-links')
 
+test('mdLinks es función', () => {
+  expect(mdLinks).toBeDefined();
+})
+
 test('Debería devolver verdadero para validar el link', () => {
   expect(mdLinks.mdLinks(1)).toEqual({
     validate: true
